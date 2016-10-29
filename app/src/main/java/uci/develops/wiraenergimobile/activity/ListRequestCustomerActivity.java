fd6fa95699c42a5394d7c7b2d8584c234d32ba46
marthinmhpakpahan <marthinmhpakpahan@gmail.com>
2016-10-29 17:02:51 +0700

    update list request and customer

--- app/src/main/java/uci/develops/wiraenergimobile/activity/ListRequestCustomerActivity.java
+++ app/src/main/java/uci/develops/wiraenergimobile/activity/ListRequestCustomerActivity.java
@@ -1,15 +1,12 @@
 package uci.develops.wiraenergimobile.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,7 +16,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import uci.develops.wiraenergimobile.R;
-import uci.develops.wiraenergimobile.adapter.RequestCustomerAdapter;
+import uci.develops.wiraenergimobile.adapter.CustomerAdapter;
 import uci.develops.wiraenergimobile.helper.SharedPreferenceManager;
 import uci.develops.wiraenergimobile.model.CustomerModel;
 import uci.develops.wiraenergimobile.response.RequestListCustomerResponse;
@@ -29,7 +26,7 @@ public class ListRequestCustomerActivity extends AppCompatActivity {
 
     RecyclerView recycleViewRequest;
     List<CustomerModel> modelRequestList;
-    RequestCustomerAdapter requestCustomerAdapter;
+    CustomerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +40,8 @@ public class ListRequestCustomerActivity extends AppCompatActivity {
         recycleViewRequest.setLayoutManager(mLayoutManager);
         recycleViewRequest.setItemAnimator(new DefaultItemAnimator());
         modelRequestList = new ArrayList<>();
-        requestCustomerAdapter = new RequestCustomerAdapter(ListRequestCustomerActivity.this, modelRequestList);
-        recycleViewRequest.setAdapter(requestCustomerAdapter);
+        customerAdapter = new CustomerAdapter(ListRequestCustomerActivity.this, modelRequestList);
+        recycleViewRequest.setAdapter(customerAdapter);
 
         Call<RequestListCustomerResponse> requestListCustomerResponseCall = RestClient.getRestClient()
                 .getAllRequestCustomer("Bearer "+new SharedPreferenceManager().getPreferences(ListRequestCustomerActivity.this, "token"));
@@ -53,9 +50,18 @@ public class ListRequestCustomerActivity extends AppCompatActivity {
             public void onResponse(Call<RequestListCustomerResponse> call, Response<RequestListCustomerResponse> response) {
                 if(response.isSuccessful()){
                     if(response.body().getData().size() > 0){
-                        Toast.makeText(ListRequestCustomerActivity.this, "Not empty", Toast.LENGTH_SHORT).show();
                         modelRequestList = response.body().getData();
-                        requestCustomerAdapter.updateList(modelRequestList);
+                        List<CustomerModel> dataCustomer = new ArrayList<CustomerModel>();
+                        for(CustomerModel customerModel : modelRequestList){
+                            if(customerModel.getActive() == 0 && (customerModel.getApprove() == 0 || customerModel.getApprove() == 2 || customerModel.getApprove() == 3)){
+                                dataCustomer.add(customerModel);
+                            }
+                        }
+                        if(dataCustomer.size() > 0) {
+                            customerAdapter.updateList(dataCustomer);
+                        } else {
+                            Toast.makeText(ListRequestCustomerActivity.this, "Empty data", Toast.LENGTH_SHORT).show();
+                        }
                     } else {
                         Toast.makeText(ListRequestCustomerActivity.this, "Empty data", Toast.LENGTH_SHORT).show();
                     }
---
+++ app/src/main/java/uci/develops/wiraenergimobile/adapter/CustomerAdapter.java
@@ -0,0 +1,94 @@
+package uci.develops.wiraenergimobile.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import uci.develops.wiraenergimobile.R;
+import uci.develops.wiraenergimobile.activity.FormCustomerActivity;
+import uci.develops.wiraenergimobile.helper.SharedPreferenceManager;
+import uci.develops.wiraenergimobile.model.CustomerModel;
+
+/**
+ * Created by ArahitoPC on 10/24/2016.
+ */
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.MyViewHolder> {
+    private List<CustomerModel> customerModelList;
+    private Context context;
+
+    Map<String, List<String>> mRoles = new TreeMap<>();
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtIdCustomer, txtDecode, txtEmail, txtStatus;
+        public Button buttonDetail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            txtIdCustomer = (TextView) view.findViewById(R.id.txtIdCustomer);
+            txtDecode = (TextView) view.findViewById(R.id.txtDecode);
+            txtEmail = (TextView) view.findViewById(R.id.txtEmail);
+            txtStatus = (TextView) view.findViewById(R.id.txtStatus);
+            buttonDetail = (Button) view.findViewById(R.id.buttonDetail);
+        }
+    }
+
+    public CustomerAdapter(Context context, List<CustomerModel> customerModelList) {
+        this.context = context;
+        this.customerModelList = customerModelList;
+    }
+
+    public void updateList(List<CustomerModel> newList) {
+        this.customerModelList.clear();
+        this.customerModelList = newList;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView;
+        itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_request_customer2, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        final CustomerModel customerModel = customerModelList.get(position);
+        holder.txtIdCustomer.setText(": " + customerModel.getFirst_name());
+        holder.txtDecode.setText(": " + customerModel.getDecode());
+        holder.txtEmail.setText(": " + customerModel.getEmail());
+
+        if (customerModel.getActive() == 1 && customerModel.getApprove() == 1) {
+            holder.txtStatus.setText("Activated");
+        } else {
+            holder.txtStatus.setText("Inactive");
+        }
+
+        holder.buttonDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * Set decode melalui data per row
+                 * menampung di shared preference
+                 */
+                new SharedPreferenceManager().setPreferences(context, "customer_decode", ""+customerModel.getDecode());
+                Intent intent = new Intent(context, FormCustomerActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return customerModelList.size();
+    }
+}
