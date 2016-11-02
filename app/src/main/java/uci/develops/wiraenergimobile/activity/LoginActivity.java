15f01d0264c58ffde084a13aa27e7fb3fcb36425
Uci Arahito Lubis <arahitolubis@gmail.com>
2016-11-02 13:34:43 +0700

    Update notif feedback

--- app/src/main/java/uci/develops/wiraenergimobile/activity/LoginActivity.java
+++ app/src/main/java/uci/develops/wiraenergimobile/activity/LoginActivity.java
@@ -131,6 +131,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             //Toast.makeText(LoginActivity.this, activated + "  " + token, Toast.LENGTH_SHORT).show();
                             new SharedPreferenceManager().setPreferences(LoginActivity.this, "token", token);
                             new SharedPreferenceManager().setPreferences(LoginActivity.this, "customer_decode", customer_decode);
+                            new SharedPreferenceManager().setPreferences(LoginActivity.this, "is_login", "true");
                             if (activated == true){
                                 Call<ListRoleResponse> listRoleResponseCall = RestClient.getRestClient().getAllRoles("Bearer "+token);
                                 listRoleResponseCall.enqueue(new Callback<ListRoleResponse>() {
