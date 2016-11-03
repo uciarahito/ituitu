20c861c839fdd529e049212ddd7e6324e90fb39f
marthinmhpakpahan <marthinmhpakpahan@gmail.com>
2016-11-03 15:32:12 +0700

    home activity with fragment

---
+++ app/src/main/java/uci/develops/wiraenergimobile/activity/HomeActivity.java
@@ -0,0 +1,96 @@
+package uci.develops.wiraenergimobile.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uci.develops.wiraenergimobile.R;
+import uci.develops.wiraenergimobile.fragment.FragmentInventory;
+import uci.develops.wiraenergimobile.fragment.FragmentMasterSetup;
+import uci.develops.wiraenergimobile.fragment.FragmentPurchasing;
+import uci.develops.wiraenergimobile.fragment.FragmentReporting;
+import uci.develops.wiraenergimobile.fragment.FragmentSales;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        setupTabIcons();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        /**
+         * Jika dia seorang planner,maka akan ada 1 tab tambahan yaitu Pending
+         * Pending merupakan order yang telah planner assign ke driver
+         * namun driver belum mengconfirm order tersebut
+         */
+        adapter.addFragment(new FragmentMasterSetup(), "");
+        adapter.addFragment(new FragmentPurchasing(), "");
+        adapter.addFragment(new FragmentSales(), "");
+        adapter.addFragment(new FragmentInventory(), "");
+        adapter.addFragment(new FragmentReporting(), "");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_tab_setup);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_tab_purchasing);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_tab_sales);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_tab_inventory);
+        tabLayout.getTabAt(4).setIcon(R.drawable.ic_tab_report);
+    }
+
+}
---
+++ app/src/main/java/uci/develops/wiraenergimobile/fragment/FragmentMasterSetup.java
@@ -0,0 +1,66 @@
+package uci.develops.wiraenergimobile.fragment;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uci.develops.wiraenergimobile.R;
+
+public class FragmentMasterSetup extends Fragment {
+
+    public FragmentMasterSetup(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view;
+        view = inflater.inflate(R.layout.fragment_master_setup, container, false);
+
+        // start listening for refresh local file list in
+        return view;
+        //return inflater.inflate(R.layout.activity_ongoing_order, container, false);
+    }
+
+    private void initializeComponent(){
+    }
+
+    /**
+     * ketika activity resume
+     * melakukan register broadcast receiver
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
